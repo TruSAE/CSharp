7010b265dcedcf5bf73270b57f8c1a3c61d97cc1
AlexT <aletrus@yandex.ru>
2019-02-22 17:06:36 +0300

    метод минимум из трех

---
+++ GeekBrainsCSharp_1_2/GeekBrainsCSharp_1_2/MinimumVal.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeekBrainsCSharp_1_2
+{
+    class MinimumVal
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите целое число:");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите целое число:");
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            MinimumOfThree(a, b, c);
+            Console.ReadKey();
+        }
+
+        private static void MinimumOfThree(int a, int b, int c)
+        {
+            if (a <= b && a <= c)
+            {
+                Console.WriteLine($"Минимальное введенное число - {a}");
+            }
+            else
+            {
+                if (b <= a && b <= c)
+                {
+                    Console.WriteLine($"Минимальное введенное число - {b}");
+                }
+                else
+                {
+                    if (c <= b && c <= a)
+                    {
+                        Console.WriteLine($"Минимальное введенное число - {c}");
+                    }
+                }
+            }
+        }
+    }
+}
